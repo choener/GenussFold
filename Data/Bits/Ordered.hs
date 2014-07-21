295b2332b76346f0ceb9bb9ec7bf6d8c95be7472
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2014-07-21 12:31:33 +0200

    ordered by popcount with sorting
    return set elements ordered by bit popcount. sorts elements using vector
    algorithms and requires n*log n time and n space, where n=2^bits

--- Data/Bits/Ordered.hs
+++ Data/Bits/Ordered.hs
@@ -8,13 +8,48 @@
 -- The enumerator can be inlined with @unfoldr@ (of the @vector@ package)
 -- and is a good producer.
 
-module Data.Bits.Ordered where
+module Data.Bits.Ordered 
+  ( popCntSorted
+  , popCntMemo
+  ) where
+
+import           Data.Bits
+import qualified Data.Vector.Unboxed as VU
+import           Data.Vector.Unboxed (Unbox)
+import qualified Data.Vector.Algorithms.Intro as AI
+import           Data.Ord (comparing)
+import           Control.Arrow
 
 
 
--- | Given the full set (which is of the form @n==2^z-1@!) and the current
--- element, yield either @Just@ the new element and a new seed or
--- @Nothing@.
+-- The /slow/ default implementation. We sort the vector, not the list, as
+-- sorting will walk the whole data structure anyway, and the vector
+-- requires not as much memory.
+--
+-- Uses a stable sort so that @a,b@ with equal pop count still has @a<b@ in
+-- the order via @comparing (popCount &&& id)@.
 
-orderedBits :: () => a -> a -> Maybe (a,a)
-orderedBits n c = undefined
+popCntSorted :: (Unbox n, Integral n, Bits n) => Int -> VU.Vector n
+popCntSorted n = VU.modify (AI.sortBy (comparing (popCount &&& id))) $ VU.enumFromN 0 (2^n)
+{-# INLINE popCntSorted #-}
+
+-- | Memoized version @m@ is the size of the memotable (up to 31 bits are
+-- memoized), @n@ the requested number of bits.
+--
+-- NOTE Since this uses @popCntSorted@ for now it will still require a lot
+-- of memory for sorting the vector!
+
+popCntMemo :: Int -> Int -> VU.Vector Int
+popCntMemo m n
+  | n>m       = popCntSorted n
+  | m>limit   = error $ "for safety reasons, memoization is only performed for popcounts up to " ++ show limit ++ " bits, memoize manually!"
+  | otherwise = _popCntMemo !! n
+  where limit = 31
+{-# INLINE popCntMemo #-}
+
+-- | Memoizes popcount arrays. The limit to memoization is enforced by
+-- popCntMemo, not here.
+
+_popCntMemo = map popCntSorted [0..]
+{-# NOINLINE _popCntMemo #-}
+
---
+++ Data/Bits/Ordered/QuickCheck.hs
@@ -0,0 +1,5 @@
+
+-- | Check a number of properties for popcount-ordered elements.
+
+module Data.Bits.Ordered.QuickCheck where
+
