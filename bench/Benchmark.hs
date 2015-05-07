2e23a42cbcf4345a2c7ca44ff0e1f434db9e6c9d
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-05-07 13:27:18 +0200

    first test of travis-ci

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -13,22 +13,22 @@ main = defaultMain
   [ bgroup "Int"  [ bench "08" $ whnf (VU.sum . popCntSorted :: Int  -> Int )  8
                   , bench "16" $ whnf (VU.sum . popCntSorted :: Int  -> Int ) 16
                   , bench "20" $ whnf (VU.sum . popCntSorted :: Int  -> Int ) 20
-                  , bench "24" $ whnf (VU.sum . popCntSorted :: Int  -> Int ) 24
+--                  , bench "24" $ whnf (VU.sum . popCntSorted :: Int  -> Int ) 24
                   ]
   , bgroup "Word" [ bench "08" $ whnf (VU.sum . popCntSorted :: Int  -> Word)  8
                   , bench "16" $ whnf (VU.sum . popCntSorted :: Int  -> Word) 16
                   , bench "20" $ whnf (VU.sum . popCntSorted :: Int  -> Word) 20
-                  , bench "24" $ whnf (VU.sum . popCntSorted :: Int  -> Word) 24
+--                  , bench "24" $ whnf (VU.sum . popCntSorted :: Int  -> Word) 24
                   ]
   , bgroup "MemoInt"  [ bench "08" $ whnf (VU.sum . popCntMemoInt  :: Int  -> Int )  8
                       , bench "16" $ whnf (VU.sum . popCntMemoInt  :: Int  -> Int ) 16
                       , bench "20" $ whnf (VU.sum . popCntMemoInt  :: Int  -> Int ) 20
-                      , bench "24" $ whnf (VU.sum . popCntMemoInt  :: Int  -> Int ) 24
+--                      , bench "24" $ whnf (VU.sum . popCntMemoInt  :: Int  -> Int ) 24
                       ]
   , bgroup "MemoWord" [ bench "08" $ whnf (VU.sum . popCntMemoWord :: Int  -> Word)  8
                       , bench "16" $ whnf (VU.sum . popCntMemoWord :: Int  -> Word) 16
                       , bench "20" $ whnf (VU.sum . popCntMemoWord :: Int  -> Word) 20
-                      , bench "24" $ whnf (VU.sum . popCntMemoWord :: Int  -> Word) 24
+--                      , bench "24" $ whnf (VU.sum . popCntMemoWord :: Int  -> Word) 24
                       ]
 --  , bgroup "small ops" [ bench "
 --                       ]
