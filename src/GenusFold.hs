2b829f71e7f409828f12385754235e042cce037c
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-07-06 01:03:05 +0200

    GenusFold : Nussinov with pseudoknots

--- src/GenusFold.hs
+++ src/GenusFold.hs
@@ -1,8 +1,32 @@
 
 module Main where
 
+import System.Console.CmdArgs
+import Control.Monad (forM_)
+import Text.Printf
 
+import BioInf.GenusFold
+
+
+
+data Options
+  = Nussinov
+    { coopts :: Int
+    }
+  deriving (Show,Data,Typeable)
+
+oNussinov = Nussinov
+  { coopts = 1 &= help "number of co-optimal backtracking results"
+  }
 
 main :: IO ()
-main = return ()
+main = do
+  o <- cmdArgs $ modes [oNussinov]
+  case o of
+    Nussinov{..} -> do
+      ls <- lines <$> getContents
+      forM_ ls $ \l -> do
+        let (r,bs) = pseudoNussinovPairMax coopts l
+        printf "%s   %d\n" l r
+        forM_ bs $ \[b] -> printf "%s   %d\n" b r
 
