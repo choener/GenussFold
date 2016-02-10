a5c17661d01ce5123e2c85c4acec8df770acf132
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-02-10 23:00:53 +0100

    Rewrote the way we generate static forests
    much simpler, I think. Also: Post-Order forests now a bugfree.

--- Data/Forest/Static.hs
+++ Data/Forest/Static.hs
@@ -1,4 +1,6 @@
 
+-- | A data structure for a static forest.
+
 module Data.Forest.Static where
 
 import           Data.Graph.Inductive.Basic
@@ -20,7 +22,11 @@ data TreeOrder = Pre | Post
 
 
 
--- | A static forest structure...
+-- | A static forest structure. While traversals are always explicitly
+-- possible by following the indices, the nodes themselves shall always be
+-- ordered by the type @p :: TreeOrder@. This is not completely enforced,
+-- given that @Forest@ is exporting the constructor, but encouraged via
+-- construction with helper functions.
 
 data Forest (p :: TreeOrder) v a where
   Forest :: (VG.Vector v a) =>
@@ -36,60 +42,98 @@ deriving instance (Show a, Show (v a)) => Show (Forest p v a)
 
 
 
--- |
---
--- TODO write addIndicesF to allow us to remove the non-nice 'error'
--- function / 'undefined'.
---
--- TODO Explain what this function does.
+-- | Construct a static 'Forest' with a tree traversal function. I.e.
+-- @forestWith preorderF trees@ will construct a pre-order forest from the
+-- list of @trees@.
 
-forestWith :: (VG.Vector v a) => (forall a . [T.Tree a] -> [a]) -> [T.Tree a] -> Forest (p::TreeOrder) v a
+forestWith :: Show a => (VG.Vector v a) => (forall a . [T.Tree a] -> [a]) -> [T.Tree a] -> Forest (p::TreeOrder) v a
 forestWith f ts
-  = Forest { label    = VG.fromList $ f ss
-           , parent   = VU.fromList $ map (\(_,k,_,_) -> k) $ f rs
-           , children = V.fromList $ map (\(_,_,cs,_) -> VU.fromList cs) $ f rs
-           , lsib     = VU.fromList $ map fst $ tail $ S.elems sb
-           , rsib     = VU.fromList $ map snd $ tail $ S.elems sb
-           , roots    = VU.fromList . map fst . maybe [] fst . uncons . tail $ T.levels rr -- T.levels rr !! 1
+  = Forest { label    = VG.fromList $ f ts
+           , parent   = VU.fromList $ map (\(_,k,_,_) -> k) $ f pcs
+           , children = V.fromList $ map (\(_,_,cs,_) -> VU.fromList cs) $ f pcs
+           , lsib     = VU.fromList $ map fst $ S.elems lr
+           , rsib     = VU.fromList $ map snd $ S.elems lr
+           , roots    = VU.fromList $ map (fst . T.rootLabel) us
            }
   where
-    ss = ts
-    rs = relationsF (-1) $ addIndicesF 0 ss
-    rr = addIndices (-1) $ T.Node (error "forestWith :: this should have been legal") ts
-    sb = siblings rr
+    -- Step 1: construct a forest isomorphic to @ts@ but labelled with
+    -- a total order of unique identifiers. (That is: label with @Int@s).
+    -- The unique identifiers are in pre-order.
+    ps = addIndicesF' 0 ts
+    -- Step 2: use @f@ to produce a permutation map and apply this
+    -- permutation to turn the pre-order @ps@ into the required order.
+    -- Looks weird, but once we have the @bp@ indices, we need /them/ in
+    -- the correct form, hence backpermute them.
+    backp = let bp = VU.fromList $ f ps :: VU.Vector Int
+            in  VU.backpermute bp bp
+    -- Step 3: decorate the forest with indices in the correct order. Keep
+    -- the label in @snd@.
+    us = map (fmap (\(k,l) -> (backp VG.! k,l))) $ addIndicesF 0 ts
+    -- Step 4: add the correct relations (children, lrSibling, parents)
+    pcs = parentChildrenF (-1) us
+    -- A map with the left and right sibling
+    lr  = lrSiblingF us
 
 
-forestPre :: (VG.Vector v a) => [T.Tree a] -> Forest Pre v a
+
+-- | Construct a pre-ordered forest.
+
+forestPre :: (VG.Vector v a, Show a) => [T.Tree a] -> Forest Pre v a
 forestPre = forestWith preorderF
 
+-- | Construct a post-ordered forest.
 
-forestPost :: (VG.Vector v a) => [T.Tree a] -> Forest Post v a
+forestPost :: (VG.Vector v a, Show a) => [T.Tree a] -> Forest Post v a
 forestPost = forestWith postorderF
 
+-- | Add @pre-ordered@ !!! indices. First argument is the starting index.
 
 addIndices :: Int -> T.Tree a -> T.Tree (Int,a)
 addIndices k = snd . mapAccumL (\i e -> (i+1, (i,e))) k
+
+-- | Add @pre-ordered@ !!! indices, but to a forest.
 
 addIndicesF :: Int -> [T.Tree a] -> [T.Tree (Int,a)]
 addIndicesF k = snd . mapAccumL go k
   where go = mapAccumL (\i e -> (i+1, (i,e)))
 
-relationsF :: Int -> [T.Tree (Int,a)] -> [T.Tree (Int,Int,[Int],a)]
-relationsF k ts = [ T.Node (i,k,children sf,l) (relationsF i sf)  | T.Node (i,l) sf <- ts ]
+-- | Add @pre-ordered@ !!! indices to a forest, but throw the label away as
+-- well.
+
+addIndicesF' :: Int -> [T.Tree a] -> [T.Tree Int]
+addIndicesF' k = snd . mapAccumL go k
+  where go = mapAccumL (\i e -> (i+1, i))
+
+-- | Add parent + children information. Yields
+-- @(Index,Parent,[Child],Label)@. Parent is @-1@ if root node.
+
+parentChildrenF :: Int -> [T.Tree (Int,a)] -> [T.Tree (Int,Int,[Int],a)]
+parentChildrenF k ts = [ T.Node (i,k,children sf,l) (parentChildrenF i sf)  | T.Node (i,l) sf <- ts ]
   where children sf = map (fst . T.rootLabel) sf
 
-siblings :: T.Tree (Int,a) -> S.Map Int (Int,Int)
-{- -- because, well, stupid
-siblings = S.fromList . concatMap (go . map fst) . T.levels
-  where go xs = zipWith3 (\l x r -> (x,(l,r))) ((-1):xs) xs (tail xs ++ [-1])
-        go :: [Int] -> [(Int,(Int,Int))]
--}
-siblings = S.fromList . map splt . T.flatten . go ([]::[Int])
+-- | Return a map with all the nearest siblings for each node, for a forest.
+
+lrSiblingF :: [T.Tree (Int,a)] -> S.Map Int (Int,Int)
+lrSiblingF = S.delete (-1) . lrSibling . T.Node (-1,error "laziness in lrSiblingF broken")
+
+-- | Return a map with all the nearest siblings for each node, for a tree.
+
+lrSibling :: T.Tree (Int,a) -> S.Map Int (Int,Int)
+lrSibling = S.fromList . map splt . T.flatten . go ([]::[Int])
   where go sib (T.Node (k,lbl) frst) = let cs = [l | T.Node (l,_) _ <- frst] in T.Node (k,lbl,sib) [ go cs t | t <- frst]
+        splt (k,_,[])  = (k,(-1,-1))
         splt (k,_,sbl) = let (ls,rs) = span (/=k) sbl in (k,(last $ (-1):ls,head $ tail rs ++ [-1]))
 
+-- | Return the left-most leaf for each node.
 
-leftMostChildren :: Forest p v a -> VU.Vector Int
-leftMostChildren f = VG.map go $ VG.enumFromN 0 $ VG.length $ parent f
+leftMostLeaves :: Forest p v a -> VU.Vector Int
+leftMostLeaves f = VG.map go $ VG.enumFromN 0 $ VG.length $ parent f
   where go k = let cs = children f VG.! k
                in if VG.null cs then k else go (VG.head cs)
+
+
+{-
+test :: [T.Tree Char]
+test = [T.Node 'R' [T.Node 'a' [], T.Node 'b' []], T.Node 'S' [T.Node 'x' [], T.Node 'y' []]]
+-}
+
