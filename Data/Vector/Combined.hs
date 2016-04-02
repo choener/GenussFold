2bd8a9caff5f8e9dd14dab75a57cdba2752f861d
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-04-02 23:19:49 +0200

    rectangular with properties

--- Data/Vector/Combined.hs
+++ Data/Vector/Combined.hs
@@ -16,8 +16,8 @@ upperTriVG
   :: (Vector v a, Vector w (a,a))
   => OnDiag
   -> v a
-  -> w (a,a)
-upperTriVG d as = unfoldrN z go (0,if d == OnDiag then 0 else 1)
+  -> (Int, w (a,a))
+upperTriVG d as = (z, unfoldrN z go (0,if d == OnDiag then 0 else 1))
   where la = VG.length as
         z  = la * (la + if d == OnDiag then 1 else 0) `div` 2
         go (k,l)
@@ -25,4 +25,22 @@ upperTriVG d as = unfoldrN z go (0,if d == OnDiag then 0 else 1)
           | l >= la   = go (k+1,k+1 + if d == OnDiag then 0 else 1)
           | otherwise = Just ((as `VG.unsafeIndex` k, as `VG.unsafeIndex` l), (k,l+1))
 {-# Inline upperTriVG #-}
+
+-- | Outer pairing of all @as@ with all @bs@. This one is quasi-trivial,
+-- but here for completeness.
+
+rectangularVG
+  :: (Vector va a, Vector vb b, Vector w (a,b))
+  => va a
+  -> vb b
+  -> (Int, w (a,b))
+rectangularVG as bs = (z, unfoldrN z go (0,0))
+  where la = VG.length as
+        lb = VG.length bs
+        z  = la * lb
+        go (k,l)
+          | k >= la   = Nothing
+          | l >= lb   = go (k+1,0)
+          | otherwise = Just ((as `VG.unsafeIndex` k, bs `VG.unsafeIndex` l), (k,l+1))
+{-# Inline rectangularVG #-}
 
--- tests/properties.hs
+++ tests/properties.hs
@@ -12,23 +12,33 @@ import Data.Vector.Combined
 
 
 
-prop_upperTri_On :: Positive Int -> Bool
-prop_upperTri_On (Positive k) = V.toList vs == ls
-  where vs = upperTriVG OnDiag v
+prop_upperTri_On :: NonNegative Int -> Bool
+prop_upperTri_On (NonNegative k) = V.toList vs == ls
+  where vs = snd $ upperTriVG OnDiag v
         ls = [ (a,b)
              | as@(a:_) <- L.init . L.tails $ V.toList v
              , b <- as
              ]
         v = V.enumFromTo 0 k
 
-prop_upperTri_No :: Positive Int -> Bool
-prop_upperTri_No (Positive k) = V.toList vs == ls
-  where vs = upperTriVG NoDiag v
+prop_upperTri_No :: NonNegative Int -> Bool
+prop_upperTri_No (NonNegative k) = V.toList vs == ls
+  where vs = snd $ upperTriVG NoDiag v
         ls = [ (a,b)
              | (a:as) <- L.init . L.tails $ V.toList v
              , b <- as
              ]
         v = V.enumFromTo 0 k
+
+prop_rectangular :: NonNegative Int -> NonNegative Int -> Bool
+prop_rectangular (NonNegative k) (NonNegative l) = V.toList vs == ls
+  where vs = snd $ rectangularVG as bs
+        ls = [ (a,b)
+             | a <- V.toList as
+             , b <- V.toList bs
+             ]
+        as = V.enumFromTo 0 k
+        bs = V.enumFromTo 0 l
 
 main :: IO ()
 main = $(defaultMainGenerator)
