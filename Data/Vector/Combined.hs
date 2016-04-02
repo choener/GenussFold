01b63a7ec4dd8d18630e6ab962111d2081c2b3b1
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-04-02 23:06:16 +0200

    added test for 'combined'

--- Data/Vector/Combined.hs
+++ Data/Vector/Combined.hs
@@ -1,3 +1,28 @@
 
 module Data.Vector.Combined where
 
+import Data.Vector.Generic as VG
+
+
+
+-- | Shall we combine elements on the main diagonal as well?
+
+data OnDiag = OnDiag | NoDiag
+  deriving (Eq)
+
+-- | Upper triangular elements.
+
+upperTriVG
+  :: (Vector v a, Vector w (a,a))
+  => OnDiag
+  -> v a
+  -> w (a,a)
+upperTriVG d as = unfoldrN z go (0,if d == OnDiag then 0 else 1)
+  where la = VG.length as
+        z  = la * (la + if d == OnDiag then 1 else 0) `div` 2
+        go (k,l)
+          | k >= la   = Nothing
+          | l >= la   = go (k+1,k+1 + if d == OnDiag then 0 else 1)
+          | otherwise = Just ((as `VG.unsafeIndex` k, as `VG.unsafeIndex` l), (k,l+1))
+{-# Inline upperTriVG #-}
+
--- tests/properties.hs
+++ tests/properties.hs
@@ -1,8 +1,35 @@
 
 module Main where
 
+import Data.List as L
+import Data.Vector as V
+import Test.QuickCheck
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.TH
 
+import Data.Vector.Combined
+
+
+
+prop_upperTri_On :: Positive Int -> Bool
+prop_upperTri_On (Positive k) = V.toList vs == ls
+  where vs = upperTriVG OnDiag v
+        ls = [ (a,b)
+             | as@(a:_) <- L.init . L.tails $ V.toList v
+             , b <- as
+             ]
+        v = V.enumFromTo 0 k
+
+prop_upperTri_No :: Positive Int -> Bool
+prop_upperTri_No (Positive k) = V.toList vs == ls
+  where vs = upperTriVG NoDiag v
+        ls = [ (a,b)
+             | (a:as) <- L.init . L.tails $ V.toList v
+             , b <- as
+             ]
+        v = V.enumFromTo 0 k
 
 main :: IO ()
-main = return ()
+main = $(defaultMainGenerator)
 
