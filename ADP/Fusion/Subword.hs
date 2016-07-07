7c45123af742588170f3c593d38c5d5e23081e6f
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-07-07 15:57:24 +0200

    moved all Subword-related modules here
    context-free grammars on strings now require this module. this makes the
    core of ADPfusion smaller.
    
    linear grammars on strings remain in the core module. The reason is that
    we need *one* non-trivial data type for performance measurements easily
    available. Points are, hopefully, enough

---
+++ ADP/Fusion/Subword.hs
@@ -0,0 +1,23 @@
+
+-- | This module re-exports the @ADP.Fusion@ module and imports all
+-- instances defined for @Subword@s.
+--
+-- It should, in general, be enough to import this module to be able to
+-- write context-free grammars on strings.
+
+module ADP.Fusion.Subword
+  ( module ADP.Fusion
+  ) where
+
+import ADP.Fusion
+import ADP.Fusion.Base.Subword
+import ADP.Fusion.SynVar.Array.TermSymbol.Subword
+import ADP.Fusion.SynVar.Indices.Subword
+import ADP.Fusion.SynVar.Recursive.Subword
+import ADP.Fusion.SynVar.Split.Subword
+import ADP.Fusion.Term.Chr.Subword
+import ADP.Fusion.Term.Deletion.Subword
+import ADP.Fusion.Term.Epsilon.Subword
+import ADP.Fusion.Term.PeekIndex.Subword
+import ADP.Fusion.Term.Strng.Subword
+
