485c22b48842f8f42d8c52ae6deeebfc21cf57ba
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-06-12 15:36:42 +0200

    updated default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,14 @@
-{ mkDerivation, base, bits, criterion, primitive, QuickCheck
-, stdenv, tasty, tasty-quickcheck, tasty-th, vector
-, vector-algorithms
-}:
-mkDerivation {
-  pname = "OrderedBits";
-  version = "0.0.1.4";
-  src = ./.;
-  libraryHaskellDepends = [
-    base bits primitive vector vector-algorithms
-  ];
-  testHaskellDepends = [
-    base QuickCheck tasty tasty-quickcheck tasty-th vector
-  ];
-  benchmarkHaskellDepends = [ base criterion vector ];
-  homepage = "https://github.com/choener/OrderedBits";
-  description = "Efficient ordered (by popcount) enumeration of bits";
-  license = stdenv.lib.licenses.bsd3;
+with (import <nixpkgs> {});
+with haskell.lib;
+
+rec {
+  hsPkgs = haskellPackages.extend (packageSourceOverrides {
+  });
+  hsShell = with hsPkgs; shellFor {
+    packages = p: [ p.OrderedBits ];
+    withHoogle = true;
+    buildInputs = [
+      cabal-install ghc
+    ];
+  };
 }
