ed2ffe8e69b3abf0891b91342e9d70dbf0ed92f1
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-06-21 23:26:56 +0200

    better default.nix

--- default.nix
+++ default.nix
@@ -2,9 +2,8 @@ with (import <nixpkgs> {});
 with haskell.lib;
 
 rec {
-  hsPkgs = haskellPackages.extend (packageSourceOverrides {
-    DPutils = ../Lib-DPutils;
-  });
+  hsSrcSet = {DPutils = ./.;};
+  hsPkgs = haskellPackages.extend (packageSourceOverrides hsSrcSet);
   hsShell = with hsPkgs; shellFor {
     packages = p: [ p.DPutils ];
     withHoogle = true;
