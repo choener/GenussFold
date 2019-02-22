3b19f32b34a07c03abe5a22588cca97fb6f4794e
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-02-22 16:12:43 +0100

    new env-haskell

--- default.nix
+++ default.nix
@@ -1,2 +1,19 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, base, bits, criterion, primitive, QuickCheck
+, stdenv, tasty, tasty-quickcheck, tasty-th, vector
+, vector-algorithms
+}:
+mkDerivation {
+  pname = "OrderedBits";
+  version = "0.0.1.4";
+  src = ./.;
+  libraryHaskellDepends = [
+    base bits primitive vector vector-algorithms
+  ];
+  testHaskellDepends = [
+    base QuickCheck tasty tasty-quickcheck tasty-th vector
+  ];
+  benchmarkHaskellDepends = [ base criterion vector ];
+  homepage = "https://github.com/choener/OrderedBits";
+  description = "Efficient ordered (by popcount) enumeration of bits";
+  license = stdenv.lib.licenses.bsd3;
+}
