125fb73f5fcd0cbee483dcd6d75abfab2d3e7f13
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-08-05 10:39:52 +0200

    fastmath comments

--- lib/Data/Ord/Fast.hs
+++ lib/Data/Ord/Fast.hs
@@ -5,6 +5,8 @@
 -- @min@ operations that are branchless.
 --
 -- This should be temporary, since GHC is supposed to get the branchless variants.
+--
+-- NOTE these do not seem to be faster anyway.
 
 module Data.Ord.Fast where
 
@@ -18,17 +20,35 @@ class FastMinMax x where
 
 instance FastMinMax Int where
   fastmin x' y' =
-    let I# x = coerce x' ∷ Int
-        I# y = coerce y' ∷ Int
+    let I# x = x'
+        I# y = y'
         l    = x <# y
-        res  = I# (  (x *# l) +# (y *# (1# -# l))  )
-    in  coerce res
+        --res  = I# (  (x *# l) +# (y *# (1# -# l))  )
+        res  = I# ( x +# (x -# y) `andI#` uncheckedIShiftRA# (x -# y) 63# )
+    in  res
   {-# Inline fastmin #-}
   fastmax x' y' =
-    let I# x = coerce x' ∷ Int
-        I# y = coerce y' ∷ Int
+    let I# x = x'
+        I# y = y'
         l    = x <# y
-        res  = I# (  (x *# (1# -# l)) +# (y *# l)  )
-    in  coerce res
+        --res  = I# (  (x *# (1# -# l)) +# (y *# l)  )
+        res  = I# ( x -# (x -# y) `andI#` uncheckedIShiftRA# (x -# y) 63# )
+    in  res
   {-# Inline fastmax #-}
+
+--instance FastMinMax Double where
+--  fastmin x' y' =
+--    let D# x = x'
+--        D# y = y'
+--        l    = 1## -- x <## y
+--        res  = D# 1# -- (  (x *## l) +## (y *## (1## -## l))  )
+--    in  res
+--  {-# Inline fastmin #-}
+--  fastmax x' y' =
+--    let D# x = x'
+--        D# y = y'
+--        l    = 1## -- x <## y
+--        res  = D# 1# -- (  (x *## (1## -## l)) +## (y *## l)  )
+--    in  res
+--  {-# Inline fastmax #-}
 
