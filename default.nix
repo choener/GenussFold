155fe04b227dcc3c11fa28da2380d0a27fe088d6
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-08-09 11:16:02 +0200

    big cleanup in preparation to update

--- default.nix
+++ default.nix
@@ -1,25 +1,15 @@
-{ mkDerivation, ADPfusion, base, BenchmarkHistory, bits, containers
-, DPutils, mmorph, mtl, OrderedBits, primitive, PrimitiveArray
-, QuickCheck, stdenv, strict, tasty, tasty-quickcheck, tasty-th
-, template-haskell, th-orphans, transformers, tuple, vector
+{ mkDerivation, ADPfusion, base, bits, containers, DPutils, mmorph
+, mtl, OrderedBits, primitive, PrimitiveArray, QuickCheck, stdenv
+, strict, template-haskell, th-orphans, transformers, tuple, vector
 }:
 mkDerivation {
   pname = "ADPfusionSubword";
   version = "0.0.0.1";
   src = ./.;
-  isLibrary = true;
-  isExecutable = true;
   libraryHaskellDepends = [
     ADPfusion base bits containers DPutils mmorph mtl OrderedBits
     primitive PrimitiveArray QuickCheck strict template-haskell
     th-orphans transformers tuple vector
-  ];
-  testHaskellDepends = [
-    ADPfusion base bits OrderedBits PrimitiveArray QuickCheck strict
-    tasty tasty-quickcheck tasty-th vector
-  ];
-  benchmarkHaskellDepends = [
-    base BenchmarkHistory PrimitiveArray vector
   ];
   homepage = "https://github.com/choener/ADPfusionSubword";
   description = "Efficient, high-level dynamic programming for CFGs on strings";
