e50c48ab34ccdadb911629015e8a7f5e84f5f6dc
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2020-03-27 22:51:36 +0100

    housekeeping Info instances

--- lib/Data/Info.hs
+++ lib/Data/Info.hs
@@ -4,9 +4,21 @@
 
 module Data.Info where
 
+import Data.List (concat,intersperse)
+import qualified Data.Vector.Unboxed as VU
+
 
 
 class Info c where
   -- | The string returned by 'info' should be around 60 chars per line, and one line if possible.
   info :: c -> String
+
+instance (Info a, Info b, Info c) => Info (a,b,c) where
+  info (a,b,c) = concat $ intersperse " " [info a, info b, info c]
+
+instance (VU.Unbox a, Info a) => Info (VU.Vector a) where
+  info = concatMap info . VU.toList
+
+instance Info Int where
+  info = show
 
