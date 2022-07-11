14ac1345a38536d77288ae540bc9d01fdf75e38d
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-07-11 19:21:40 +0200

    successful fusion for Genussfold

--- BioInf/GenussFold/PKN.hs
+++ BioInf/GenussFold/PKN.hs
@@ -35,16 +35,17 @@ import BioInf.GenussFold.PKN.Grammar
 
 
 
-bpmax :: Monad m => SigPKN m Int Int Char -- Char
+bpmax :: Monad m => SigPKN m Int Int Char Char
 {-# Inline bpmax #-}
 bpmax = SigPKN
   { unp = const
-  , jux = \ x c y d -> if c `pairs` d then x + y + 1 else -987654
---  , pse = \ () () x y -> x + y
+  , jux = \ x c y d -> x + y -- x c y d -> if c `pairs` d then x + y + 1 else -987654
+  , pse = \ () () x y -> x + y
   , nil = \ ()      -> 0
+  , pkk = \ (Z:.x:.()) (Z:.a:.()) y (Z:.():.z) (Z:.():.b) -> -888888 -- if a `pairs` b then x + y + z + 1 else -888888
 --  , pkk = \ (Z:.x:.()) (Z:.a:.()) y (Z:.():.z) (Z:.():.b) -> -888888 -- if a `pairs` b then x + y + z + 1 else -888888
-  , pkk = \(Z:.x:.()) -> x-777777
---  , nll = \ (Z:.():.()) -> 0
+--  , pkk = \(Z:.x:.()) -> x-777777
+  , nll = \ (Z:.():.()) -> 0
   , idd = \x -> x-666666
   , h   = SM.foldl' max (-999999)
   }
@@ -66,18 +67,18 @@ pairs !c !d
 -- or something isomorphic. While [String] works, it allows for too many
 -- possibilities here! ([] ist lightweight, on the other hand ...)
 
-pretty :: Monad m => SigPKN m [String] [[String]] Char -- Char
-{-# Inline pretty #-}
-pretty = SigPKN
-  { unp = \ [x] c     -> [x ++ "."]
-  , jux = \ [x] c [y] d -> [x ++ "(" ++ y ++ ")"]
---  , pse = \ () () [x1,x2] [y1,y2] -> [x1 ++ y1 ++ x2 ++ y2]
-  , nil = \ ()      -> [""]
---  , pkk = \ (Z:.[x]:.()) (Z:.a:.()) [y1,y2] (Z:.():.[z]) (Z:.():.b) -> [x ++ "[" ++ y1 , y2 ++ z ++ "]"]
---  , nll = \ (Z:.():.()) -> ["",""]
-  , idd = id
-  , h   = SM.toList
-  }
+--pretty :: Monad m => SigPKN m [String] [[String]] Char -- Char
+--{-# Inline pretty #-}
+--pretty = SigPKN
+--  { unp = \ [x] c     -> [x ++ "."]
+--  , jux = \ [x] c [y] d -> [x ++ "(" ++ y ++ ")"]
+----  , pse = \ () () [x1,x2] [y1,y2] -> [x1 ++ y1 ++ x2 ++ y2]
+--  , nil = \ ()      -> [""]
+----  , pkk = \ (Z:.[x]:.()) (Z:.a:.()) [y1,y2] (Z:.():.[z]) (Z:.():.b) -> [x ++ "[" ++ y1 , y2 ++ z ++ "]"]
+----  , nll = \ (Z:.():.()) -> ["",""]
+--  , idd = id
+--  , h   = SM.toList
+--  }
 
 -- |
 --
@@ -115,7 +116,7 @@ runInsideForward i = runST $ do
         (ITbl @_ @_ @_ @_ @_ @_ (Z:.EmptyOk:.EmptyOk) arrUU)
         (ITbl @_ @_ @_ @_ @_ @_ (Z:.EmptyOk:.EmptyOk) arrVV)
         (chr i)
---        (chr i)
+        (chr i)
   return tbls
 
 type X' = TwITblBt 0 0 (Dense VU.Vector) EmptyOk (Subword I) Int Id Id [String]
